94d4c5702d87a0a4ab48c8801218e9bfbd37614f
Darius-Ru <103390874+Darius-Ru@users.noreply.github.com>
2022-04-29 19:14:15 -0400

    Create DecayTest.cpp
    Test code for particle system. After applying the ParticleDecay function, the lorentz vector of the particles is set to what the simulation did. The angles of each particle, as well as the energy, can be accessed.

---
+++ ParticleSystem/bin/DecayTest.cpp
@@ -0,0 +1,26 @@
+/*
+Darius Rushaj
+
+Testing ParticleSystem class.
+*/
+
+#include"particle.h"
+#include"ParticleSystem.h"
+
+int main()
+{
+	particle Lambda("Lambda"), Proton("Proton"), Pion("Pion"); //Creating the particles.
+
+	ParticleSystem LambdaDecaySystem(Lambda, Proton, Pion); //Creating the system.
+
+	LambdaDecaySystem.ParticleDecay(5); //Applying MC to system with initial lambada momentum of 5;
+
+	Lambda.Print(); Proton.Print(); Pion.Print(); //Printing the particles themselves. Note: I used the TLorentzVector print function in the print function for my particle
+												  //class, so there is a (p, eta, phi, E) vector as well.
+
+	LambdaDecaySystem.Print();	//Using system print function. This is just for testing.
+
+	std::cout<<"Proton Phi: "<<Proton.GetPhi()<<std::endl; //All angles and energies were also set.
+
+	return 0;
+}
